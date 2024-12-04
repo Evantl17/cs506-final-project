08db975c00170e5815c6a398ccabb9175bfe3357
Thomas Boles <thomas211738@gmail.com>
2024-12-03 23:19:41 -0500

    updated csvs

--- csvWriting.py
+++ csvWriting.py
@@ -24,9 +24,32 @@ for ticker in sp500_tickers:
 df = pd.DataFrame(data)
 
 # Save the DataFrame to a CSV file
+with open('sp500_tickers_full_info.csv', 'w') as file:
+    pass
 df.to_csv('sp500_tickers_full_info.csv', index=False)
 
 print("CSV file 'sp500_tickers_full_info.csv' has been created.")
+
+
+# Create a CSV file with closing prices for each ticker
+
+csv_output = 'closingPrices.csv'
+prices = pd.read_csv(csv_output)
+tickers = prices['Ticker'].unique()
+
+with open(csv_output, 'w') as file:
+    pass
+
+write_header = True
+
+for ticker in tickers:
+    stock_df = yf.Ticker(ticker).history(period='1Y')
+    stock_df['Ticker'] = ticker
+    company = stock_df[['Ticker', 'Close']]
+    company['Date'] = pd.to_datetime(company.index)
+    company.to_csv(csv_output, mode='a', header=write_header, index=False)
+    write_header = False
+    
 
 
 
